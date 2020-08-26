deef8d3dc2106b0ba267dbc5d855e8a0edbcc566
arthurbike <arthur.7martins@gmail.com>
2020-08-26 19:47:30 -0300

    Base criada

--- autentication/src/repositories/index.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from "typeorm";
-import { userRepository, User } from "./userRepository";
-
-export { userRepository };
--- autentication/src/repositories/userRepository.ts
+++
@@ -1,5 +0,0 @@
-import { getRepository, Repository } from "typeorm";
-import { User } from "../entity/User";
-
-export let userRepository: Repository<User> = getRepository(User);
-export { User };
