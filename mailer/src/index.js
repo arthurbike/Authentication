8392931242001fdb23ec30faed53d79e7da58645
arthurbike <arthur.7martins@gmail.com>
2020-09-26 11:18:04 -0300

    .

--- mailer/src/index.js
+++
@@ -1,9 +0,0 @@
-require("./app");
-
-if (!process.env.SMTP_PORT) {
-  throw new Error("Enviroment variable SMTP_PORT not setted");
-}
-
-if (!process.env.SERVER_PORT) {
-  throw new Error("Enviroment variable SERVER_PORT not setted");
-}
